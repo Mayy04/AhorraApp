ce7ba0c242aa7f73353a06c6dfdeaede006d9e34
Mayy04 <124048822@upq.edu.mx>
2025-10-27 10:01:42 -0600

    Hoja de estilos para las diferentes pantallas

---
+++ styles.js
@@ -0,0 +1,76 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#009c5bff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#D9D9D9',
+    padding: 10,
+    marginBottom: 20,
+    width: '60%',
+    backgroundColor: '#fff',
+    borderRadius:10,
+  },
+  texto: {
+    color: '#000000',
+    fontSize: 30,
+    marginBottom: 30,
+    fontWeight: 'bold',
+  },
+  texto2: {
+    color: '#000000',
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  texto3: {
+    color: '#BEFFE0',
+    fontSize: 15,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  texto4: {
+    color: '#FFDD00',
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  logo: {
+    width: 85,
+    height: 85,
+  },
+   lineaHorizontal: {
+    height: 1,
+    width: '80%', 
+    backgroundColor: '#fff',
+  },
+  botones:{
+    backgroundColor:'#00D162',
+    paddingVertical:10,
+    paddingHorizontal:25,
+    borderRadius:8,
+    marginBottom:20,
+    width: '60%',
+  },
+  botones2:{
+    backgroundColor:'#009c5bff',
+    paddingVertical:10,
+    paddingHorizontal:25,
+    borderRadius:8,
+    marginBottom:20,
+    width: '60%',
+  },
+  textoBoton:{
+    color:'#fff',
+    fontSize:16,
+    fontWeight:'600',
+    textAlign:'center',
+  },
+  espacioBlanco:{
+    fontSize:9
+  },
+});
