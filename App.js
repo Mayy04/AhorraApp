00e59512fc22f87ecb19da0e2489adf5395c32a9
FERMINHDZ <124049799@upq.edu.mx>
2025-11-27 20:19:46 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,3 +4,9 @@ export default function App() {
     <InicioScreen/>
   );
 }
+import MenuScreen from './Screens/menuScreen.js';
+export default function App() {
+  return (
+    <MenuScreen/>
+  );
+}
