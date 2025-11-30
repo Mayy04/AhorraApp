9dcfa042f01cb7e6e85228691bf5d843aafa8e47
FERMINHDZ <124049799@upq.edu.mx>
2025-11-30 08:43:51 -0600

    se le dio orden y estilos a cambio de contraseña

--- Screens/RegistroScreen.js
+++ Screens/RegistroScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, ScrollView } from "react-native";
-import UsuarioService from "../Services/usuarioService";
+
 
 export default function RegistroScreen({ navigation, setUsuarioLogueado }) {
 
